49c4f50fd15abee915faddfd7194e509d8dd76ac
Stewart Wingfield <sww915@gmail.com>
2024-01-19 18:56:39 -0500

    added few components

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import Login from "./components/Login";
 import { Link } from "react-router-dom";
 import cookie from "cookie";
 import Register from "./components/Register";
+import Navigation from "./components/Navigation";
 
 const checkAuth = () => {
   const cookies = cookie.parse(document.cookie);
@@ -19,6 +20,7 @@ const ProtectedRoute = (props) => {
 const Router = () => {
   return (
     <>
+      <Navigation />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
---
+++ src/components/Navigation.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+import cookie from "cookie";
+
+const Navigation = () => {
+  const navigate = useNavigate();
+
+  return (
+    <AppBar position="relative" style={{ backgroundColor: "#00b36b" }}>
+      <Toolbar>
+        <Typography
+          variant="h6"
+          style={{ flexGrow: "1", textDecoration: "None" }}
+        >
+          <Link to="/">Austin Small Business</Link>
+        </Typography>
+        <ul className="nav-list">
+          <li className="nav-list-item">
+            <Link to="/">Listings</Link>
+          </li>
+
+          {document.cookie.includes("loggedIn=true") ? (
+            <li className="nav-list-item">
+              <Link to="/add">Add</Link>
+            </li>
+          ) : null}
+
+          <li
+            className="nav-list-item"
+            onClick={() => {
+              document.cookie = cookie.serialize("loggedIn", null, {
+                maxAge: 0,
+              });
+              props.logOutUser();
+              navigate("/login");
+            }}
+          >
+            {document.cookie === "" ? <Link to="/login">Login</Link> : "Logout"}
+          </li>
+        </ul>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navigation;
