11175e7c9251591d3a527512364c9bd3911829f6
Stewart Wingfield <sww915@gmail.com>
2024-01-25 20:13:21 -0500

    add few buttons

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React from "react";
 import "./App.css";
 import Router from "./Router";
 import { BrowserRouter } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import { Button, Stack } from "react-bootstrap";
+import { Button } from "bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
+      <Container>
+        <Stack direction="horizontal" gap="2" className="mb-4">
+          <h1 className="me-auto">Budgets</h1>
+          <Button variant="primary">Add Budget</Button>
+          <Button variant="primary">Add Expense</Button>
+        </Stack>
+      </Container>
       <Router />
     </BrowserRouter>
   );
